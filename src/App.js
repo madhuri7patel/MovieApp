7ef3546654dca0bdf977d34a2547304cd7f5c7bf
Yash Sathwara <ysdeveloper1998@gmail.com>
2024-07-03 11:18:00 +0530

    Branch merged into master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import AddEditMoviePage from './components/AddEditPage';
-import MovieDetailsPage from './components/MoviedetailPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import AddEditMoviePage from "./components/AddEditPage";
+import MovieDetailsPage from "./components/MovieDetailPage/MoviedetailPage";
 
 function App() {
   return (
